1a17fe90ca76e3dea69979b9424342a0521e520f
James Lawson <jameslawson@users.noreply.github.com>
2016-09-04 07:51:19 +0100

    Move matchers into /matcher
    - Update require statements

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-var tag = require('./matchers/tag');
-var klass = require('./matchers/class');
-var classes = require('./matchers/classes');
-var style = require('./matchers/style');
-var children = require('./matchers/children');
-var text = require('./matchers/text');
-var attribute = require('./matchers/attribute');
-
 function plugin(chai, utils) {
   var Assertion = chai.Assertion;
 
-  Assertion.addMethod('tag', tag(chai, utils));
-  Assertion.addMethod('class', klass(chai, utils));
-  Assertion.addMethod('classes', classes(chai, utils));
-  Assertion.addMethod('style', style(chai, utils));
-  Assertion.addMethod('children', children(chai, utils));
-  Assertion.addMethod('text', text(chai, utils));
-  Assertion.addMethod('attribute', attribute(chai, utils));
+  Assertion.addMethod('tag',       require('./matcher/tag')(chai, utils));
+  Assertion.addMethod('class',     require('./matcher/class')(chai, utils));
+  Assertion.addMethod('classes',   require('./matcher/classes')(chai, utils));
+  Assertion.addMethod('style',     require('./matcher/style')(chai, utils));
+  Assertion.addMethod('children',  require('./matcher/children')(chai, utils));
+  Assertion.addMethod('text',      require('./matcher/text')(chai, utils));
+  Assertion.addMethod('attribute', require('./matcher/attribute')(chai, utils));
 }
 
 module.exports = plugin;
